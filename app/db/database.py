5d15cc0e370b61d4ab67a6949c29064813983cdc
Marius <marius.podlisnic@student.usv.ro>
2026-08-13 13:26:23 +0300

    Database sessions

--- app/db/database.py
+++ app/db/database.py
@@ -1,3 +1,16 @@
+from collections.abc import Generator
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session,sessionmaker
+from sqlalchemy.orm import Session, sessionmaker
+from app.config import settings
+DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(bind=engine,autoflush=False,autocommit=False)
+
+def get_db() -> Generator[Session,None,None]:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
